7ebbdf58c1ce16b94240beaf0c6a94b0449237c8
Dylan DeBriere <dbd249@miami.edu>
2025-05-18 15:52:53 -0400

    init

---
+++ include/Size.h
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+
+class Size
+{
+public:
+	static unsigned int windowWidth;
+	static unsigned int windowHeight;
+
+	static sf::Vector2f playerSize;
+	static float ballSize;
+};
---
+++ src/Size.cpp
@@ -0,0 +1,6 @@
+#include "../include/Size.h"
+
+float Size::ballSize = 25;
+sf::Vector2f Size::playerSize = { 40.f, 300.f };
+unsigned int Size::windowWidth = 1920;
+unsigned int Size::windowHeight = 1080;
