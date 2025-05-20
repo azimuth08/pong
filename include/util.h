ce6408a6e2f358c17cbdfe97e4ca95e319e6d789
Dylan DeBriere <dbd249@miami.edu>
2025-05-19 22:14:47 -0400

    added v0 collision detection and in range (didnt use)

--- include/util.h
+++ include/util.h
@@ -2,9 +2,13 @@
 #define UTIL_H_
 
 #include <cmath>
+#include <SFML/Graphics.hpp>
 
 namespace util {
-
+    float getDistance(sf::Shape&, sf::Shape&);
+    bool checkCollision(sf::CircleShape&, sf::RectangleShape&);
+    bool inRange(sf::Shape&, sf::Shape&);
+    void bounce();
 
 }
 
--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,30 @@
 #include "../include/util.h"
 
+namespace util {
 
+    float getDistance(sf::Shape& s1, sf::Shape& s2) {
+        return 0.0;
+    }
+
+    bool checkCollision(sf::CircleShape& ball, sf::RectangleShape& player) {
+        bool ret = false;
+        sf::FloatRect playerBounds = player.getGlobalBounds();
+
+        sf::Vector2f ballLHS = {ball.getPosition().x - ball.getRadius(), ball.getPosition().y}; 
+        sf::Vector2f ballRHS = {ball.getPosition().x + ball.getRadius(), ball.getPosition().y}; 
+        
+        // if left or right most point of ball in player, collision has been made
+        if(playerBounds.contains(ballLHS) || playerBounds.contains(ballRHS) ) {
+            ret = true;
+        }
+        
+        return ret;
+    }
+
+    bool inRange(float x1, float x2, float k) {
+        return k > x1 && k < x2;
+    }
+}
 
 
 
