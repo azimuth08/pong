68fa6a28da6f03765b284b856960a9d12b2d0696
Dylan DeBriere <dbd249@miami.edu>
2025-05-29 15:44:19 -0400

    changed size of paddles

--- src/Size.cpp
+++ src/Size.cpp
@@ -1,6 +1,6 @@
 #include "../include/Size.h"
 
 float Size::ballSize = 25;
-sf::Vector2f Size::playerSize = { 40.f, 300.f };
+sf::Vector2f Size::playerSize = { 25.f, 200.f };
 unsigned int Size::windowWidth = 1920;
 unsigned int Size::windowHeight = 1080;
